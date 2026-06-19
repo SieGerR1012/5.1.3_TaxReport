9ebe5a3224336acaf6139bf503ea2e587aab476c
DmitryK96 <sieger1012@gmail.com>
2026-06-19 12:26:13 +0700

    Реализация работы программы в Main

--- src/Main.java
+++ src/Main.java
@@ -9,4 +9,31 @@ void main() throws InterruptedException {
     int[] shop3 = generateRevenueArray(random);
 
     LongAdder totalRevenue = new LongAdder();
+
+    Thread store1 = new RevenueCalculator("Магазин №1", shop1, totalRevenue);
+    Thread store2 = new RevenueCalculator("Магазин №2", shop2, totalRevenue);
+    Thread store3 = new RevenueCalculator("Магазин №3", shop3, totalRevenue);
+
+    System.out.println("Начинаем подсчет выручки...\n");
+
+    store1.start();
+    store2.start();
+    store3.start();
+
+    store1.join();
+    store2.join();
+    store3.join();
+
+    System.out.println("\nОбщий отчет для налоговой: " + totalRevenue.sum());
+}
+
+private static int[] generateRevenueArray(Random random) {
+
+    int[] array = new int[ARRAY_SIZE];
+
+    for (int i = 0; i < array.length; i++) {
+        array[i] = random.nextInt(MAX_REVENUE) + 1;
+    }
+
+    return array;
 }
